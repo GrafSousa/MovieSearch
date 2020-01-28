4e13e5ab7cf7638ab7482a00fafe2104d79879d1
Gustavo <graf.sousa@gmail.com>
2020-01-28 20:30:49 -0300

    Adiciona estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>Create React App</h1>;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 }
 
 
